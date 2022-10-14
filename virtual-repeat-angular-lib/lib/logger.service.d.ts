4e3d51e473b4080e024fd898190a45410b071c4c
FURA\gerard <gcarbo@miraiblau.com>
2022-10-14 18:54:26 +0200

    Auto-generated commit [ci skip]

--- virtual-repeat-angular-lib/lib/logger.service.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class LoggerService {
-    log: any;
-    constructor();
-    static ɵfac: i0.ɵɵFactoryDeclaration<LoggerService, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<LoggerService>;
-}
